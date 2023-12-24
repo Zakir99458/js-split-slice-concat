221b90e54212d9abce052cee9585bdb151ae7042
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-24 15:27:57 +0200

    string-concat

--- string-slice.js
+++ string-slice.js
@@ -1,3 +1,17 @@
 const anthem = "amar sonar bangla ami tumai valobashi";
 const words = anthem.split(' ');
-console.log(words);
+// console.log(words);
+const splitWithA = anthem.split('a');
+// console.log(splitWithA);
+const stringSlice = anthem.slice(5,14);
+// console.log(stringSlice);
+const firsName = "Mohammad";
+const lastName = " Hossain";
+const fullName = firsName + lastName;
+// console.log(fullName);
+// concat
+const myFullName = firsName.concat(' Zakir').concat(' Hossain');
+console.log(myFullName);
+// join
+const testString = ['a', 'b', 'c'];
+console.log(testString.join(''));
